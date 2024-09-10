b281415e9da1e90d0f2106c0d6fa5da2f637ab88
Raghav Nayak <ragsnayak@gmail.com>
2024-09-10 21:21:06 +0530

    updated canConstruct code

--- src/dynamicProgramming/memoization/canConstruct/canConstruct.go
+++ src/dynamicProgramming/memoization/canConstruct/canConstruct.go
@@ -19,19 +19,62 @@ empty string generation takes 0 elements from the array
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
-func canConstruct(target string, wordBank []string) bool {
+func canConstructWithoutMemo(target string, wordBank []string) bool {
 	if target == "" {
 		return true
 	}
 
-	return true
+	for word := range wordBank {
+		index := strings.Index(target, wordBank[word])
+		if index == 0 {
+			suffix := target[len(wordBank[word]):]
+			if canConstructWithoutMemo(suffix, wordBank) {
+				return true
+			}	
+		}
+	}
+
+	return false
 }
 
+func canConstructWithMemo(target string, wordBank []string, memo map[string]bool) bool {
+	if _, ok := memo[target]; ok {
+			return memo[target]
+	}
+	if target == "" {
+		return true
+	}
+
+	for word := range wordBank {
+		index := strings.Index(target, wordBank[word])
+		if index == 0 {
+			suffix := target[len(wordBank[word]):]
+			if canConstructWithMemo(suffix, wordBank, memo) {
+				memo[target] = true
+				return true
+			}	
+		}
+	}
+
+	memo[target] = false
+	return false
+}
+
+
 func main() {
-	fmt.Println(canConstruct("abcdef", []string{"ab", "abc", "cd", "def", "abcd"}))
+	fmt.Println(canConstructWithoutMemo("abcdef", []string{"ab", "abc", "cd", "def", "abcd"})) // true
+	fmt.Println(canConstructWithoutMemo("skateboard", []string{"bo", "rd", "ate", "t", "ska", "sk", "boar"})) // false
+	fmt.Println(canConstructWithoutMemo("enterapotentpot", []string{"a", "p", "ent", "enter", "ot", "o", "t"})) // true
+	// fmt.Println(canConstructWithoutMemo("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", []string{"e", "ee", "eee", "eeee", "eeeee", "eeeeee"})) // false
 
-	fmt.Println(canConstruct("skateboard", []string{"bo", "rd", "ate", "t", "ska", "sk", "boar"}))
 
+	fmt.Println(canConstructWithMemo("abcdef", []string{"ab", "abc", "cd", "def", "abcd"}, map[string]bool{})) // true
+	fmt.Println(canConstructWithMemo("skateboard", []string{"bo", "rd", "ate", "t", "ska", "sk", "boar"}, map[string]bool{})) // false
+	fmt.Println(canConstructWithMemo("enterapotentpot", []string{"a", "p", "ent", "enter", "ot", "o", "t"}, map[string]bool{})) // true
+	fmt.Println(canConstructWithMemo("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", []string{"e", "ee", "eee", "eeee", "eeeee", "eeeeee"}, map[string]bool{})) // false
 }
